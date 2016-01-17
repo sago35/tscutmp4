6a6791b1537bdba6bf0e6d5a36ec7ab2a595f653
sago35 <sago35@gmail.com>
2016-01-17 21:53:50 +0900

    D&Dでリストボックスに登録

--- tscutmp4.go
+++ tscutmp4.go
@@ -4,25 +4,68 @@ import (
 	"fmt"
 	"github.com/lxn/walk"
 	. "github.com/lxn/walk/declarative"
+	"log"
 )
 
 type MyMainWindow struct {
 	*walk.MainWindow
+	model *EnvModel
+	lb    *walk.ListBox
+}
+
+type EnvItem struct {
+	name  string
+	value string
+}
+
+type EnvModel struct {
+	walk.ListModelBase
+	items []EnvItem
 }
 
 func main() {
-	mw := &MyMainWindow{}
+	mw := &MyMainWindow{model: NewEnvModel()}
 
-	MainWindow{
+	if _, err := (MainWindow{
 		AssignTo: &mw.MainWindow,
-		Title:   fmt.Sprintf("hello walk"),
-		MinSize: Size{320, 240},
+		Title:    fmt.Sprintf("hello walk"),
+		MinSize:  Size{320, 240},
+		Size:     Size{320, 240},
+		Layout:   VBox{MarginsZero: true},
 		OnDropFiles: func(files []string) {
 			fmt.Println("-- dropped --")
 			for _, f := range files {
 				fmt.Println(f)
+				mw.model.items = append(mw.model.items, EnvItem{name: f})
 			}
+			mw.lb.SetModel(mw.model)
+
 			fmt.Println("--")
 		},
-	}.Run()
+		Children: []Widget{
+			VSplitter{
+				Children: []Widget{
+					ListBox{
+						AssignTo: &mw.lb,
+						Model:    mw.model,
+					},
+				},
+			},
+		},
+	}.Run()); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func NewEnvModel() *EnvModel {
+	m := &EnvModel{items: make([]EnvItem, 0)}
+	return m
+}
+
+func (m *EnvModel) ItemCount() int {
+	return len(m.items)
+}
+
+func (m *EnvModel) Value(index int) interface{} {
+	return m.items[index].name
 }
